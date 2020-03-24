a30f939f097878bfa1580bb4033528533e55ee4f
Jose <joseruiz.dev@gmail.com>
2020-03-24 02:06:08 -0600

    post registered in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from blog.models import Post
+from django.contrib.auth.models import User
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'excerpt',
+        'author',
+        'status',
+        'created_at',
+    )
--- blog/models.py
+++ blog/models.py
@@ -8,8 +8,9 @@ STATUS = (
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
+    excerpt = models.TextField(max_length=500, null=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts'),
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     status = models.IntegerField(choices=STATUS, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
